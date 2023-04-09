52814894cd80df5c487beb2bad733b07b038d61f
Stefanos Orovas <stef.orovas@gmail.com>
2023-04-09 17:17:50 +0300

    installed credo

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule Typerace.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:horde, "~> 0.8.7"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
